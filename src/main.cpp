21665b7f774a3712138d1e27617e28bae4e8fdc9
Tadeas Maca <maca.tadeas@gmail.com>
2026-02-15 10:58:52 +0100

    testing in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "rud/base/string.hpp"
+#include "rud/ds/vector.hpp"
 #include "rud/os/std_in.hpp"
 #include "rud/os/std_out.hpp"
+#include "rud/os_low/system.hpp"
 
 using namespace rud::ds;
 using namespace rud;
@@ -9,19 +11,24 @@ using namespace rud::os_low;
 
 int main (int argc, char *argv[]) {
     StdIn in = StdIn::make();
+    StdOut out = StdOut::make();
 
-    debug_print(Lit("your name: "));
+    out.write_str(Lit("what is your name?\n")).or_panic();
 
     AllocString name = in.read_line().or_panic();
 
-    AllocString str = AllocString::make();
-    str.push_copy(Lit("hello, "))
-        ->push_copy(name);
-    
-    debug_print(str);
+    AllocString format = AllocString::make();
+    format.push_copy(Lit("hello, "));
+    format.push_copy(name);
 
+    out.write_str(format).or_panic();
+
+    format.destroy();
     name.destroy();
-    str.destroy();
+    
+    Vector<String> args = Vector<String>::make();
+    run_process(Lit("ls"), args.to_linear_view());
+    args.destroy();
 
     return 0;
 }

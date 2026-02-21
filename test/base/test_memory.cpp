8eb02badeacdcbf6d44b860ff19366a0a00d086c
Tadeas Maca <maca.tadeas@gmail.com>
2026-02-21 12:02:05 +0100

    memory tests

--- test/base/test_memory.cpp
+++ test/base/test_memory.cpp
@@ -11,396 +11,288 @@
 
 using namespace rud;
 
-SCENARIO("trying to allocate an arbitrary number of bytes works correctly", "[try_allocate_size]") {
-    GIVEN("0 bytes") {
+TEST_CASE("trying to allocate an arbitrary number of bytes works correctly", "[try_allocate_size]") {
+    SECTION("allocation of 0 bytes") {
         constexpr u64 bytes = 0;
 
-        WHEN("allocating them") {
-            Result<void*, AllocError> result = try_allocate_size(bytes);
+        Result<void*, AllocError> result = try_allocate_size(bytes);
 
-            THEN("allocation is successful") {
-                REQUIRE(result.ok);
-            }
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(result.unwrap() != nullptr);
-            }
-
-            deallocate(result.unwrap());
-        }
-    }
-
-    GIVEN("1 byte") {
-        constexpr u64 bytes = 1;
-
-        WHEN("allocating it") {
-            Result<void*, AllocError> result = try_allocate_size(bytes);
-
-            THEN("allocation is successful") {
-                REQUIRE(result.ok);
-            }
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(result.unwrap() != nullptr);
-            }
-            
-            deallocate(result.unwrap());
-        }
-    }
-
-    GIVEN("an arbitrary number of bytes") {
-        u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 2048)));
-
-
-        WHEN("allocating it") {
-            Result<void*, AllocError> result = try_allocate_size(bytes);
-
-            THEN("allocation is successful") {
-                REQUIRE(result.ok);
-            }
-            
-            THEN("the returned pointer is not null") {
-                REQUIRE(result.unwrap() != nullptr);
-            }
-            
-            deallocate(result.unwrap());
-        }
-    }
-
-    GIVEN("too much bytes to allocate (u64 max)") {
-        constexpr u64 bytes = std::numeric_limits<u64>::max();
-
-        WHEN("allocating it") {
-            Result<void*, AllocError> result = try_allocate_size(bytes);
-
-            THEN("allocation is not successful") {
-                REQUIRE(!result.ok);
-            }
-        }
-    }
-}
-
-SCENARIO("allocating an arbitrary number of bytes works correctly", "[allocate_size]") {
-    GIVEN("0 bytes") {
-        constexpr u64 bytes = 0;
-
-        WHEN("allocating them") {
-            bool success = true;
-            void* result = nullptr;
-            try {
-                result = allocate_size(bytes);
-            } catch(AllocError& e) {
-                success = false;
-            }
-
-            THEN("allocation is successful") {
-                REQUIRE(success);
-            }
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(result != nullptr);
-            }
-
-            deallocate(result);
-        }
-    }
-
-    GIVEN("1 byte") {
-        constexpr u64 bytes = 1;
-
-        WHEN("allocating it") {
-            bool success = true;
-            void* result = nullptr;
-            try {
-                result = allocate_size(bytes);
-            } catch(AllocError& e) {
-                success = false;
-            }
-
-            THEN("allocation is successful") {
-                REQUIRE(success);
-            }
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(result != nullptr);
-            }
-            
-            deallocate(result);
-        }
-    }
-
-    GIVEN("an arbitrary number of bytes") {
-        u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 2048)));
-
-
-        WHEN("allocating it") {
-            bool success = true;
-            void* result = nullptr;
-            try {
-                result = allocate_size(bytes);
-            } catch(AllocError& e) {
-                success = false;
-            }
-
-            THEN("allocation is successful") {
-                REQUIRE(success);
-            }
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(result != nullptr);
-            }
-            
-            deallocate(result);
-        }
-    }
-
-    GIVEN("too much bytes to allocate (u64 max)") {
-        constexpr u64 bytes = std::numeric_limits<u64>::max();
-
-        WHEN("allocating it") {
-            bool success = true;
-            try {
-                allocate_size(bytes);
-            } catch(AllocError& e) {
-                success = false;
-            }
-
-            THEN("allocation is not successful") {
-                REQUIRE(!success);
-            }
-        }
-    }
-}
-
-SCENARIO("allocating a structure or a type works correctly", "[try_allocate]") {
-    GIVEN("an integer") {
-        constexpr u32 integer = 10;
-
-        WHEN("allocating space for it and copying it into the memory") {
-            Result<u32*, AllocError> result = try_allocate(integer);
-
-            THEN("allocation is successful") {
-                REQUIRE(result.ok);
-            }
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(result.unwrap() != nullptr);
-            }
-
-            THEN("the memory at the pointer holds the same value as the integer") {
-                REQUIRE(*result.unwrap() == integer);
-            }
-            
-            deallocate(result.unwrap());
-        }
-    }
-
-    GIVEN("an empty structure foo") {
-        struct Foo {} foo;
-
-        WHEN("allocating space for it and copying it into the memory") {
-            Result<Foo*, AllocError> result = try_allocate(foo);
-
-            THEN("allocation is successful") {
-                REQUIRE(result.ok);
-            }
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(result.unwrap() != nullptr);
-            }
-            
-            deallocate(result.unwrap());
-        }
-    }
-}
-
-SCENARIO("allocation of a structure or a type works correctly", "[allocate]") {
-    GIVEN("an integer") {
-        constexpr u32 integer = 10;
-
-        WHEN("allocating space for it and copying it into the memory") {
-            u32* result = allocate(integer);
-
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(result != nullptr);
-            }
-
-            THEN("the memory at the pointer holds the same value as the integer") {
-                REQUIRE(*result == integer);
-            }
-            
-            deallocate(result);
-        }
-    }
-
-    GIVEN("an empty structure foo") {
-        struct Foo {} foo;
-
-        WHEN("allocating space for it and copying it into the memory") {
-            Foo* result = allocate(foo);
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(result != nullptr);
-            }
-            
-            deallocate(result);
-        }
-    }
-}
-
-SCENARIO("trying to reallocate an arbitrary number of bytes works correctly", "[try_reallocate]") {
-    GIVEN("a previously allocated memory block with some data") {
-        u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 1024)));
-        void* ptr = allocate_size(bytes);
-        mem_set(ptr, 0xAB, bytes);
+        REQUIRE(result.ok);
+        REQUIRE(result.unwrap() != nullptr);
         
-        void* ptr_save = allocate_size(bytes);
-        mem_copy(ptr_save, ptr, bytes);
+        deallocate(result.unwrap());
+    }
 
-        WHEN("reallocating it to 0 bytes") {
-            Result<void*, AllocError> r_new_ptr = try_reallocate(ptr, 0);
-            
-            THEN("the allocation is successful") {
-                REQUIRE(r_new_ptr.ok);
+    SECTION("allocation of 1 byte") {
+        constexpr u64 bytes = 1;
+
+        Result<void*, AllocError> result = try_allocate_size(bytes);
+
+        REQUIRE(result.ok);
+        REQUIRE(result.unwrap() != nullptr);
+        
+        deallocate(result.unwrap());
+    }
+
+    SECTION("allocation of an arbitrary number of bytes") {
+        u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 2048)));
+
+        Result<void*, AllocError> result = try_allocate_size(bytes);
+
+        REQUIRE(result.ok);
+        REQUIRE(result.unwrap() != nullptr);
+        
+        deallocate(result.unwrap());
+    }
+
+    SECTION("alocation of too much bytes to allocate (u64 max)") {
+        constexpr u64 bytes = std::numeric_limits<u64>::max();
+
+        Result<void*, AllocError> result = try_allocate_size(bytes);
+
+        REQUIRE(!result.ok);
+    }
+}
+
+SCENARIO("push and pop works on a list", "[list]") {
+    GIVEN("an empty list if integers") {
+        C_List<u32> list;
+
+        WHEN("a value is pushed into the list") {
+            list.push(10);
+
+            THEN("the list length is equal to 1") {
+                REQUIRE(list.len() == 1);
             }
+        }
+    }
+}
 
-            THEN("the returned pointer is not null") {
-                REQUIRE(r_new_ptr.unwrap() != nullptr);
-            }
 
-            deallocate(r_new_ptr.unwrap());
+TEST_CASE("allocating an arbitrary number of bytes works correctly", "[allocate_size]") {
+    SECTION("allocation of 0 bytes") {
+        constexpr u64 bytes = 0;
+
+        bool success = true;
+        void* result = nullptr;
+        try {
+            result = allocate_size(bytes);
+        } catch(AllocError& e) {
+            success = false;
         }
 
-        WHEN("reallocating it to 1 byte") {
-            Result<void*, AllocError> r_new_ptr = try_reallocate(ptr, 1);
+        REQUIRE(success);
+        REQUIRE(result != nullptr);
             
-            THEN("the alloction is successful") {
-                REQUIRE(r_new_ptr.ok); 
-            }
+        deallocate(result);
+    }
 
-            THEN("the returned pointer is not null") {
-                REQUIRE(r_new_ptr.unwrap() != nullptr);
-            }
+    SECTION("allocating 1 byte") {
+        constexpr u64 bytes = 1;
 
-            THEN("the contents are preserved") {
-                REQUIRE(mem_equals(ptr_save, r_new_ptr.unwrap(), 1));
-            }
-            
-            deallocate(r_new_ptr.unwrap());
+        bool success = true;
+        void* result = nullptr;
+        try {
+            result = allocate_size(bytes);
+        } catch(AllocError& e) {
+            success = false;
         }
 
-        WHEN("reallocating it to a larger arbitrary size") {
-            u64 new_size = GENERATE(Catch::Generators::take(50, Catch::Generators::random(1024, 2048)));
-            Result<void*, AllocError> r_new_ptr = try_reallocate(ptr, new_size);
+        REQUIRE(success);
+        REQUIRE(result != nullptr);
             
-            THEN("the allocation is successful") {
-                REQUIRE(r_new_ptr.ok);
-            }
+        deallocate(result);
+    }
 
-            THEN("the returned pointer is not null") {
-                REQUIRE(r_new_ptr.unwrap() != nullptr);
-            }
+    SECTION("allocation of an arbitrary number of bytes") {
+        u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 2048)));
 
-            THEN("the contents are preserved") {
-                REQUIRE(mem_equals(ptr_save, r_new_ptr.unwrap(), bytes));
-            }
-
-            deallocate(r_new_ptr.unwrap());
+        bool success = true;
+        void* result = nullptr;
+        try {
+            result = allocate_size(bytes);
+        } catch(AllocError& e) {
+            success = false;
         }
 
-        WHEN("reallocating it to an extremely large size") {
-            Result<void*, AllocError> r_new_ptr = try_reallocate(ptr, std::numeric_limits<u64>::max());
-            
-            THEN("the allocation is not successful") {
-                REQUIRE(!r_new_ptr.ok);
-            }
+        REQUIRE(success);
+        REQUIRE(result != nullptr);
+        
+        deallocate(result);
+    }
 
-            deallocate(ptr);
+    SECTION("allocation of too much bytes to allocate (u64 max)") {
+        constexpr u64 bytes = std::numeric_limits<u64>::max();
+
+        bool success = true;
+        try {
+            allocate_size(bytes);
+        } catch(AllocError& e) {
+            success = false;
         }
 
+        REQUIRE(!success);
+    }
+}
+
+TEST_CASE("allocating a structure or a type works correctly", "[try_allocate]") {
+    SECTION("allocation of an integer") {
+        constexpr u32 integer = 10;
+
+        Result<u32*, AllocError> result = try_allocate(integer);
+
+        REQUIRE(result.ok);
+        REQUIRE(result.unwrap() != nullptr);
+        REQUIRE(*result.unwrap() == integer);
+        
+        deallocate(result.unwrap());
+    }
+
+    SECTION("allocation of an empty structure foo") {
+        struct Foo {} foo;
+
+        Result<Foo*, AllocError> result = try_allocate(foo);
+
+        REQUIRE(result.ok);
+        REQUIRE(result.unwrap() != nullptr);
+            
+        deallocate(result.unwrap());
+    }
+}
+
+TEST_CASE("allocation of a structure or a type works correctly", "[allocate]") {
+    SECTION("allocation of an integer") {
+        constexpr u32 integer = 10;
+
+        u32* result = allocate(integer);
+
+        REQUIRE(result != nullptr);
+        REQUIRE(*result == integer);
+        
+        deallocate(result);
+    }
+
+    SECTION("allocation an empty structure foo") {
+        struct Foo {} foo;
+
+        Foo* result = allocate(foo);
+
+        REQUIRE(result != nullptr);
+        deallocate(result);
+    }
+}
+
+TEST_CASE("trying to reallocate an arbitrary number of bytes works correctly", "[try_reallocate]") {
+    u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 1024)));
+    void* ptr = allocate_size(bytes);
+    
+    mem_set(ptr, 0xAB, bytes);
+    
+    void* ptr_save = allocate_size(bytes);
+    mem_copy(ptr_save, ptr, bytes);
+
+    SECTION("reallocation to 0 bytes") {
+        Result<void*, AllocError> r_new_ptr = try_reallocate(ptr, 0);
+            
+        REQUIRE(r_new_ptr.ok);
+        REQUIRE(r_new_ptr.unwrap() != nullptr);
+            
+        deallocate(r_new_ptr.unwrap());
+        deallocate(ptr_save);
+    }
+
+    SECTION("reallocation to 1 byte") {
+        Result<void*, AllocError> r_new_ptr = try_reallocate(ptr, 1);
+        
+        REQUIRE(r_new_ptr.ok); 
+        REQUIRE(r_new_ptr.unwrap() != nullptr);
+        REQUIRE(mem_equals(ptr_save, r_new_ptr.unwrap(), 1));
+        
+        deallocate(r_new_ptr.unwrap());
+        deallocate(ptr_save);
+    }
+
+    SECTION("reallocation to a larger arbitrary size") {
+        u64 new_size = GENERATE(Catch::Generators::take(50, Catch::Generators::random(1024, 2048)));
+        Result<void*, AllocError> r_new_ptr = try_reallocate(ptr, new_size);
+        
+        REQUIRE(r_new_ptr.ok);
+        REQUIRE(r_new_ptr.unwrap() != nullptr);
+        REQUIRE(mem_equals(ptr_save, r_new_ptr.unwrap(), bytes));
+
+        deallocate(r_new_ptr.unwrap());
+        deallocate(ptr_save);
+    }
+
+    SECTION("reallocation to an extremely large size") {
+        Result<void*, AllocError> r_new_ptr = try_reallocate(ptr, std::numeric_limits<u64>::max());
+       
+        REQUIRE(!r_new_ptr.ok);
+
+        deallocate(ptr);
         deallocate(ptr_save);
     }
 }
 
-SCENARIO("reallocation of an arbitrary number of bytes works correctly", "[reallocate]") {
-    GIVEN("a previously allocated memory block with some data") {
-        u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 1024)));
-        void* ptr = allocate_size(bytes);
-        mem_set(ptr, 0xAB, bytes);
+TEST_CASE("reallocation of an arbitrary number of bytes works correctly", "[reallocate]") {
+    u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 1024)));
+    void* ptr = allocate_size(bytes);
+    mem_set(ptr, 0xAB, bytes);
+    
+    void* ptr_save = allocate_size(bytes);
+    mem_copy(ptr_save, ptr, bytes);
+
+    SECTION("reallocation to 0 bytes") {
+        void* new_ptr = reallocate(ptr, 0);
         
-        void* ptr_save = allocate_size(bytes);
-        mem_copy(ptr_save, ptr, bytes);
+        REQUIRE(new_ptr != nullptr);
 
-        WHEN("reallocating it to 0 bytes") {
-            void* new_ptr = reallocate(ptr, 0);
-            
-            THEN("the returned pointer is not null") {
-                REQUIRE(new_ptr != nullptr);
-            }
-
-            deallocate(new_ptr);
-        }
-
-        WHEN("reallocating it to 1 byte") {
-            void* new_ptr = reallocate(ptr, 1);
-
-            THEN("the returned pointer is not null") {
-                REQUIRE(new_ptr != nullptr);
-            }
-
-            THEN("the contents are preserved") {
-                REQUIRE(mem_equals(ptr, new_ptr, 1));
-            }
-            
-            deallocate(new_ptr);
-        }
-
-        WHEN("reallocating it to a larger arbitrary size") {
-            u64 new_size = GENERATE(Catch::Generators::take(50, Catch::Generators::random(1024, 2048)));
-            void* new_ptr = reallocate(ptr, new_size);
-            
-            THEN("the returned pointer is not null") {
-                REQUIRE(new_ptr != nullptr);
-            }
-
-            THEN("the contents are preserved") {
-                REQUIRE(mem_equals(ptr_save, new_ptr, bytes));
-            }
-
-            deallocate(new_ptr);
-        }
-
-        WHEN("reallocating it to an extremely large size") {
-            REQUIRE_THROWS_AS(reallocate(ptr, std::numeric_limits<u64>::max()), AllocError);
-            
-            deallocate(ptr);
-        }
-
+        deallocate(new_ptr);
         deallocate(ptr_save);
     }
+
+    SECTION("reallocation to 1 byte") {
+        void* new_ptr = reallocate(ptr, 1);
+
+        REQUIRE(new_ptr != nullptr);
+        REQUIRE(mem_equals(ptr, new_ptr, 1));
+        
+        deallocate(new_ptr);
+        deallocate(ptr_save);
+    }
+
+    SECTION("reallocation to a larger arbitrary size") {
+        u64 new_size = GENERATE(Catch::Generators::take(50, Catch::Generators::random(1024, 2048)));
+        void* new_ptr = reallocate(ptr, new_size);
+        
+        REQUIRE(new_ptr != nullptr);
+        REQUIRE(mem_equals(ptr_save, new_ptr, bytes));
+
+        deallocate(new_ptr);
+        deallocate(ptr_save);
+    }
+
+    SECTION("reallocation to an extremely large size") {
+        REQUIRE_THROWS_AS(reallocate(ptr, std::numeric_limits<u64>::max()), AllocError);
+        
+        deallocate(ptr);
+        deallocate(ptr_save);
+    }
+
 }
 
-SCENARIO("deallocate frees memory safely", "[deallocate]") {
-    GIVEN("a previously allocated block") {
-        u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 1024)));
-        void* ptr = allocate_size(bytes);
+TEST_CASE("deallocate frees memory safely", "[deallocate]") {
+    u64 bytes = GENERATE(Catch::Generators::take(100, Catch::Generators::random(1, 1024)));
+    void* ptr = allocate_size(bytes);
 
-        WHEN("deallocating it") {
-            THEN("it does not throw") {
-                REQUIRE_NOTHROW(deallocate(ptr));
-            }
-        }
+    SECTION("deallocation") {
+        REQUIRE_NOTHROW(deallocate(ptr));
     }
 }
 
-SCENARIO("deallocate panics when deallocating nullptr", "[deallocate]") {
-    GIVEN("a null pointer") {
-        WHEN("deallocating it") {
-            THEN("it panics") {
-                REQUIRE(code_aborts([] () {deallocate(nullptr);}));
-            }
-        }
+TEST_CASE("deallocate panics when deallocating nullptr", "[deallocate]") {
+    SECTION("deallocation of a null pointer") {
+        REQUIRE(code_aborts([] () {deallocate(nullptr);}));
     }
 }
 
--- test/helpers.hpp
+++ test/helpers.hpp
@@ -1,19 +1,32 @@
 #ifndef RUD_TEST_HELPERS_HPP
 #define RUD_TEST_HELPERS_HPP
 
-#include <cstdlib>
+#include <cstdio>
 #include <sys/wait.h>
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 inline bool code_aborts(void (*func)()) {
     pid_t pid = fork();
-    if(pid == 0) {
-        // Child process
-        func();        // call the code that should abort
-        exit(0);       // if it didn't abort
+    if (pid < 0) {
+        return false;
+    } else if (pid == 0) {
+        FILE* devnull = fopen("/dev/null", "w");
+        if (devnull) {
+            dup2(fileno(devnull), STDERR_FILENO);
+            fclose(devnull);
+        }
+
+        func();      // Run the code that might abort
+        _exit(0);    // Exit normally if no abort occurred
     } else {
         // Parent process
-        int status;
+        int status = 0;
         waitpid(pid, &status, 0);
+
+        // True if child terminated via SIGABRT
         return WIFSIGNALED(status) && WTERMSIG(status) == SIGABRT;
     }
 }
